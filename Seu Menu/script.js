233b667562f5ca328721f5582573d7caa540fcc0
Matthews Brito <109427217+Matthewsfx05@users.noreply.github.com>
2023-05-05 12:38:28 -0300

    Add files via upload

---
+++ Seu Menu/script.js
@@ -0,0 +1,83 @@
+const menu = [
+    {
+        id: 1,
+        title: "Buttermilk Pancakes",
+        category: "Breakfast",
+        price: 15.99,
+        image: "./image/item-1.jpeg",
+        description: "I'm baby woke mlkshk wolf bitters live-edge blue botte, hammock freegan copper whatever cold-pressed"
+    },
+    {
+        id: 2,
+        title: "Diner Double",
+        category: "Lunch",
+        Price: 13.99,
+        image:"./image/item-2.jpeg",
+        description: "Vaporware iphone mumnlecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing.";
+    },
+    {
+        id: 3,
+        title: "Godzilla Milkshake",
+        category: "Shakes",
+        price: 6.99,
+        image:"./image/item-3.jpeg",
+        description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral",
+    },
+    {
+        id: 4,
+        title: "Country Delight",
+        category: "Breakfast",
+        price: 20.99,
+        image:"./image/item-4.jpeg",
+        description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism autin mlkshk truffaut";
+    },
+    {
+        id: 5,
+        title: "Egg Attack",
+        category: "Lunch",
+        price: 22.99,
+        image:"./image/item-5.jpeg",
+        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up",
+    },
+    {
+        id: 6,
+        title: "Oreo Dream",
+        category: "Shakes",
+        price: 18.99,
+        image:"./image/item-6.jpeg",
+        description: "Portland chicarrones ethical edison, palo santo craft beer chia heirloom iphone everyday",
+    },
+    {
+        id: 7,
+        title: "Bacon Overflow",
+        category: "Breakfast",
+        price: 8.99,
+        image:"./image/item-7.jpeg",
+        description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+    },
+    {
+        id: 8,
+        title: "American Classic",
+        category: "Lunch",
+        price: 12.99,
+        image:"./image/item-8.jpeg",
+        description: "On it tumblr kickstarter thundercast migas everyday carry squid palo santo leggings. Food truck truffaut",
+    },
+    {
+        id: 9,
+        title: "Quarantine Buddy",
+        category: "Shakes",
+        price: 16.99,
+        image:"./image/item-9.jpeg",
+        description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yucci crucifx microdosing",
+    },
+    {
+        id: 10,
+        title: "Stack Dinner",
+        category: "Dinner",
+        price: 39.99,
+        image:"./image/item-10.jpeg",
+        description: "",
+    },
+    
+];
